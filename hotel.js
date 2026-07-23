6661c8034a45efba413e6e23828c7d61fe640dd5
“kevinchavezg” <kevinchavezg@gmail.com>
2026-07-22 20:37:06 -0600

    feat: :rocket: Se añade funcion de menu.

--- hotel.js
+++ hotel.js
@@ -4,3 +4,13 @@ function timpoEspera(ms) {
     return new Promise ((resolve) => setTimeout(resolve,ms));
 }
 
+async function menu() {
+    let opcion = prompt(
+      "*** Administración Hotel ***\n" +
+        "1. Registrar nueva habitación\n" +
+        "2. Listar habitaciones\n" +
+        "3. Buscar habitación por numero\n" +
+        "4. Cambiar estado de una habitación\n" +
+        "5. Eliminar habitación\n" +
+        "6. Salir",
+    );
